5e5a13ded605f9c7473ca5c00f0b077801b716d7
Justus Krebsfänger <bewerbung_univention@protonmail.com>
2023-01-16 06:38:22 +0100

    Changed browser from chrome to edge

--- batches/run.bat
+++ batches/run.bat
@@ -1,8 +1,13 @@
 @echo off
 
-
 cd C:\Emulator\site
 TASKLIST | FINDSTR php.exe || start /b C:\Emulator\PHP\php.exe -S localhost:80
 
-cd C:\Program Files\Google\Chrome\Application\
-chrome /new-window --start-fullscreen --app=http://localhost
+Rem This code kills all running msedge .exe processes
+Rem This is to force fullscreen when running the program
+Rem If you're using Edge as a normal browser you can comment theese lines to prevent other tabs from closing
+tasklist /fi "Microsoft Edge eq msedge.exe" |find ":" > nul
+if errorlevel 1 taskkill /f /im "msedge.exe"
+
+cd "C:\Program Files (x86)\Microsoft\Edge\Application"
+msedge /new-window --start-fullscreen --app=http://localhost
--- site/index.php
+++ site/index.php
@@ -3,11 +3,12 @@
     <head>
         <meta charset="utf-8">
         <title>Emulator tool</title>
+        <link rel="icon" type="image/x-icon" href="/assets/images/ico.png">
         <link rel="stylesheet" href="./css/stylesheet.css"></link>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Share+Tech&display=swap" rel="stylesheet">
-        <script type="module" src="global.js" defer></script>
+        <script src="global.js" defer></script>
         <?php include "./php/LocalManager.php"; ?>
     </head>
     <body>
@@ -19,6 +20,7 @@
                 <div class="content">
                     <div class="filter">
                         <form action="./php/CommandRunner.php" method="post" id="runner">
+
                             <div>
                                 <label for="emu_picker">Wähle deinen Emulator:</label>
                                 <select name="emulator" form="runner" id="emu_picker">
@@ -41,41 +43,38 @@
                             </div>
 
                             <div>
-                                <!--
-                                    <label for="console_picker">Wähle deine Konsole</label>
-                                    <select name="console" form="runner" id="console_picker">
-                                        <option disabled>Nintendo</option>
-                                        <option value="gameboy">Gameboy</option>
-                                        <option value="gameboy_advance">Gameboy Advance</option>
-                                        <option value="gameboy_color">Gameboy Color</option> 
-                                        <option value="snes">SNES</option>
-                                        <option value="n64">Nintendo 64</option>
-                                        <option value="wii">Nintendo Wii</option>
-                                        <option value="ds">Nintendo DS</option>
-                                        <option value="3ds">Nintendo 3DS</option>
-                                        <option value="gamecube">Nintendo Gamecube</option>
-                                        <option value="wii-u">Nintendo Wii U</option>
-                                        <option value="switch">Nintendo Switch</option>
-                                        <option disabled>Playstation</option>
-                                        <option value="playstation-1">Playstation</option>
-                                        <option value="playstation-2">Playstation 2</option>
-                                        <option value="playstation-3">Playstation 3</option>
-                                        <option value="psp">Playstation Portable (PSP)</option>
-                                    </select>
-                                 -->
-
-                                <input type="checkbox" id="gameboy_picker" name="console" value="gameboy" style="visibility: hidden;" />
-                                <label for="gameboy_picker"><img id="gameboy_img" src="/assets/images/vader.jpg" /></label>
-
-
+                                <label for="console_picker">Wähle deine Konsole:</label>
+                                <select name="console" form="runner" id="console_picker">
+                                    <option disabled>Nintendo</option>
+                                    <option value="gameboy">Gameboy</option>
+                                    <option value="gameboy_advance">Gameboy Advance</option>
+                                    <option value="gameboy_color">Gameboy Color</option> 
+                                    <option value="snes">SNES</option>
+                                    <option value="n64">Nintendo 64</option>
+                                    <option value="wii">Nintendo Wii</option>
+                                    <option value="ds">Nintendo DS</option>
+                                    <option value="3ds">Nintendo 3DS</option>
+                                    <option value="gamecube">Nintendo Gamecube</option>
+                                    <option value="wii-u">Nintendo Wii U</option>
+                                    <option value="switch">Nintendo Switch</option>
+                                    <option disabled>Playstation</option>
+                                    <option value="playstation-1">Playstation</option>
+                                    <option value="playstation-2">Playstation 2</option>
+                                    <option value="playstation-3">Playstation 3</option>
+                                    <option value="psp">Playstation Portable (PSP)</option>
+                                </select>
                             </div>
+
                             <div>
-                                <label for="game_picker">Wähle dein Spiel aus</label>
+                                <label for="game_picker">Wähle dein Spiel aus:</label>
                                 <select name="game" form="runner" id="game_picker">
                                     <?php new LocalManager("Games"); ?>
                                 </select>
                             </div>
-                            <button type="submit">Play</button>
+
+                            <div>
+                                <button type="submit">Play</button>
+                            </div>
                         </form> 
                     </div>
                     <div class="games_content">
